274fc4d7a8f7cb82a71783c113964208a54cc1f0
Tilo Burghardt <tb2935@bristol.ac.uk>
2026-09-19 18:00:56 +0100

    Update imperative.js

--- imperative.js
+++ imperative.js
@@ -53,7 +53,7 @@ var activities = [
 //WEEK 01 - INTRO
 ["5", "in your own time",   "<a href='https://seis.bristol.ac.uk/~sh1670/LinTut.pdf' target='_blank'>THE LINUX TUTORIAL</a><hr/><a href='https://www.ole.bris.ac.uk/bbcswebdav/courses/COMS10016_2026_TB-1/content/imperative/COMS10016_Marking.pdf' target='_blank'>MARKING GUIDE</a><hr/><a href='http://people.cs.bris.ac.uk/~burghard/setup/COMS10016_PGuide.pdf' target='_blank'>GOLDEN C RULES</a>",         "", "", "0", "0"],
 // <a href='https://uob.sharepoint.com/:b:/r/teams/UnitTeams-COMS10016-2024-25-TB-1-A/Shared%20Documents/General/studycs2024.pdf?csf=1&web=1&e=7N6tVa' target='_blank'>HOW TO STUDY CS</a>
-["1", "Mon 21/09/26<br/>10am-11am<br/>CHEM BLDG LT1", "Welcome Lecture",  "https://www.ole.bris.ac.uk/bbcswebdav/courses/COMS10016_2026_TB-1/content/imperative/COMS10016_01.pdf", "", "21", "4"],
+["1", "Mon 21/09/26<br/>10am-11am<br/>CHEM BLDG LT1", "Welcome Lecture",  "https://www.ole.bris.ac.uk/bbcswebdav/courses/COMS10016_2026_TB-1/content/imperative/COMS10016_0.pdf", "", "21", "4"],
 // ["1", "Tue 17/09/24<br/>05:00pm-05:50pm<br/>CHEM BLDG LT1", "Welcome Lecture",  "https://uob.sharepoint.com/:b:/r/teams/UnitTeams-COMS10016-2024-25-TB-1-A/Shared%20Documents/General/COMS10016_00.pdf?csf=1&web=1&e=0jQgfY", "", "21", "4"],
 ["0", "Mon 21/09/26<br/>3pm-5pm<br/>MVB2.11/1.15/2.34",    "PROCEDURES &amp; PROGRAMS",       "https://www.ole.bris.ac.uk/bbcswebdav/courses/COMS10016_2026_TB-1/content/imperative/COMS10016_01.pdf", "https://mediasite.bris.ac.uk/Mediasite/Play/91d05240456c424a922dd1c8db97569d1d", "0", "2"],
 ["11","Mon 21/09/26<br/>3pm-5pm<br/>MVB2.11/1.15/2.34", "GET YOUR PC READY",  "https://uob-coms10016.github.io/2026/setup.html", "", "0", "0"],
